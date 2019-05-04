2066e8133f9a228084af884502e41b69d9915b4d
Alex Seymour <axeal@users.noreply.github.com>
2019-05-04 16:01:37 +0200

    Add ability to specify separate etcd, controlplane and worker nodes

--- main.tf
+++ main.tf
@@ -14,25 +14,83 @@ data "template_file" "userdata" {
 resource "digitalocean_droplet" "rke-all" {
   count     = "${var.count_all_nodes}"
   image     = "${var.image}"
-  name      = "${var.prefix}-rke-${count.index}-all"
+  name      = "${var.prefix}-rke-all-${count.index}"
   region    = "${var.region}"
   size      = "${var.all_size}"
   user_data = "${data.template_file.userdata.rendered}"
   ssh_keys  = "${var.ssh_keys}"
 }
 
-data "template_file" "node" {
+resource "digitalocean_droplet" "rke-etcd" {
+  count     = "${var.count_etcd_nodes}"
+  image     = "${var.image}"
+  name      = "${var.prefix}-rke-etcd-${count.index}"
+  region    = "${var.region}"
+  size      = "${var.etcd_size}"
+  user_data = "${data.template_file.userdata.rendered}"
+  ssh_keys  = "${var.ssh_keys}"
+}
+
+resource "digitalocean_droplet" "rke-controlplane" {
+  count     = "${var.count_controlplane_nodes}"
+  image     = "${var.image}"
+  name      = "${var.prefix}-rke-controlplane-${count.index}"
+  region    = "${var.region}"
+  size      = "${var.controlplane_size}"
+  user_data = "${data.template_file.userdata.rendered}"
+  ssh_keys  = "${var.ssh_keys}"
+}
+
+resource "digitalocean_droplet" "rke-worker" {
+  count     = "${var.count_worker_nodes}"
+  image     = "${var.image}"
+  name      = "${var.prefix}-rke-worker-${count.index}"
+  region    = "${var.region}"
+  size      = "${var.worker_size}"
+  user_data = "${data.template_file.userdata.rendered}"
+  ssh_keys  = "${var.ssh_keys}"
+}
+
+data "template_file" "all_nodes" {
   template = "${file("files/node.yml.tmpl")}"
   count    = "${var.count_all_nodes}"
   vars = {
     public_ip  = "${digitalocean_droplet.rke-all.*.ipv4_address[count.index]}"
+    roles       = "[controlplane,worker,etcd]"
+  }
+}
+
+data "template_file" "etcd_nodes" {
+  template = "${file("files/node.yml.tmpl")}"
+  count    = "${var.count_etcd_nodes}"
+  vars = {
+    public_ip  = "${digitalocean_droplet.rke-etcd.*.ipv4_address[count.index]}"
+    roles       = "[etcd]"
+  }
+}
+
+data "template_file" "controlplane_nodes" {
+  template = "${file("files/node.yml.tmpl")}"
+  count    = "${var.count_controlplane_nodes}"
+  vars = {
+    public_ip  = "${digitalocean_droplet.rke-controlplane.*.ipv4_address[count.index]}"
+    roles       = "[controlplane]"
+  }
+}
+
+data "template_file" "worker_nodes" {
+  template = "${file("files/node.yml.tmpl")}"
+  count    = "${var.count_worker_nodes}"
+  vars = {
+    public_ip  = "${digitalocean_droplet.rke-worker.*.ipv4_address[count.index]}"
+    roles       = "[worker]"
   }
 }
 
 data "template_file" "nodes" {
   template = "${file("files/nodes.yml.tmpl")}"
   vars {
-    nodes = "${join("",data.template_file.node.*.rendered)}"
+    nodes = "${chomp(join("",list(join("",data.template_file.all_nodes.*.rendered),join("",data.template_file.etcd_nodes.*.rendered),join("",data.template_file.controlplane_nodes.*.rendered),join("",data.template_file.worker_nodes.*.rendered))))}"
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -10,11 +10,35 @@ variable "count_all_nodes" {
   default = "1"
 }
 
+variable "count_etcd_nodes" {
+  default = "0"
+}
+
+variable "count_controlplane_nodes" {
+  default = "0"
+}
+
+variable "count_worker_nodes" {
+  default = "0"
+}
+
 variable "docker_version" {
   default = "17.03"
 }
 
 variable "all_size" {
+  default = "s-2vcpu-4gb"
+}
+
+variable "etcd_size" {
+  default = "s-2vcpu-4gb"
+}
+
+variable "controlplane_size" {
+  default = "s-2vcpu-4gb"
+}
+
+variable "worker_size" {
   default = "s-2vcpu-4gb"
 }
 
