12e61e3923f0bc38dcf97574268de8064f96bea4
Alex Seymour <axeal@users.noreply.github.com>
2020-11-23 10:25:42 +0100

    Add versions for Terraform 0.13

--- versions.tf
+++ versions.tf
@@ -1,4 +1,18 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
 }
